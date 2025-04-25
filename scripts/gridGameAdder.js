41e2ac5484d3f34d34b6fafc5e28545bb7f54afc
hackerFaruk <onuralparslan97@gmail.com>
2025-04-25 23:32:11 +0300

    fixed he fetch problem

---
+++ scripts/gridGameAdder.js
@@ -0,0 +1,17 @@
+fetch('scripts/games.json')
+    .then(res => res.json()) // fetchin resultunu jsona çevirdik
+    .then(games => {  // gelen resultu games olarak aldık 
+        const grid = document.getElementById('gamesUpper'); // bağlı olduğu dokümandan game-gridi aldık
+
+        games.forEach(game => {
+            const card = document.createElement('div'); // oyuna ait cardlar oluşturuldu oyun görselleri buraya eklencek
+            card.className = 'gameCard';
+            card.innerHTML = `
+        <a href="gamePage.html?id=${game.id}">
+          <img src="${game.image}" alt="${game.name}" />
+          <p>${game.name}</p>
+        </a>
+      `;
+            grid.appendChild(card);
+        });
+    });
